b1534661cbaab3bef98a4b896718252883261f64
SANTHOSH RAMARAPU <ramarapusantthosh24@gmail.com>
2023-06-13 20:09:18 +0530

    first commit

--- script.js
+++ script.js
@@ -15,5 +15,19 @@ document.querySelector('#cart-btn').onclick = () =>{
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+let  navbar= document.querySelector('.navbar');
 
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
 
